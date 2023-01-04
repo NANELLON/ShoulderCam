e898cb36b743e18668b5ff61910729c58699b4b7
Ruslan Libin <ruslanlib97@gmail.com>
2023-01-04 10:36:42 +0200

    2.0

--- Main.cs
+++ Main.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using GTA;
 using GTA.Native;
 using GTA.Math;
-using System.Security.AccessControl;
 using System.Windows.Forms;
+
 
 namespace AimCam
 {
@@ -20,61 +16,33 @@ namespace AimCam
         int replaceCam;
         bool mode;
         int delay;
-        float sidetoside = 0.26f;
-        float backtofront = -0.2f;
-        float rotation = 90.0f;
-        Keys switchKey;
+        float sidetoside = 0.19f;
+        float backtofront = -0.07f;
+        float rotation = 85f;
         public Main()
         {
            deathHealth = Settings.GetValue("SETTINGS", "deathHealth", 0.0f);
-           switchKey = Settings.GetValue("SETTINGS", "Key", Keys.X);
            replaceCam = Settings.GetValue("SETTINGS", "replaceCam", 2);
            mode = Settings.GetValue("SETTINGS", "aimOnly", false);
            delay = Settings.GetValue("SETTINGS", "resetDelay", 3000);
            Tick += onTick;
-           KeyUp += onKeyUp;
 
         }
-        void onKeyUp(object sender, KeyEventArgs e)
+        Vector3 rayCast()
         {
-            Ped player = Game.Player.Character;
-            if (cameraSet && !player.IsInVehicle()) 
-            {
-                if (e.KeyCode == switchKey && sidetoside >= 0.4f)
-                {
-                    while (sidetoside > -0.5f)
-                    {
-                        Function.Call(Hash._ATTACH_CAM_TO_PED_BONE_2, headCam, player, 31086, 0.0f, rotation, 0.0f, sidetoside, backtofront, 0.07f, true);
-                        //GTA.UI.Screen.ShowSubtitle("side:" + sidetoside + "back:" + backtofront, 2000);
-                        sidetoside = sidetoside - 0.1f;
-                        backtofront = backtofront + 0.016f;
-                        rotation = rotation - 0.5f;
-                        if (sidetoside < -0.5f)
-                        {
-                            sidetoside = -0.5f;
-                        }
-                        Wait(1);
-                    }
-                }
-                else if (e.KeyCode == switchKey && sidetoside <= -0.5f)
-                {
-                    while (sidetoside < 0.4f)
-                    {
-                        Function.Call(Hash._ATTACH_CAM_TO_PED_BONE_2, headCam, player, 31086, 0.0f, rotation, 0.0f, sidetoside, backtofront, 0.07f, true);
-                        //GTA.UI.Screen.ShowSubtitle("side:" + sidetoside + "back:" + backtofront, 2000);
-                        sidetoside = sidetoside + 0.1f;
-                        backtofront = backtofront - 0.016f;
-                        rotation = rotation + 0.5f;
-                        if (sidetoside > 0.4f)
-                        {
-                            sidetoside = 0.4f;
-                        }
-                        Wait(1);
-                    }
-                }
-            }
-           
+            //get Aim Postion
+            Vector3 camPos = Function.Call<Vector3>(Hash.GET_GAMEPLAY_CAM_COORD);
+            Vector3 camRot = Function.Call<Vector3>(Hash.GET_GAMEPLAY_CAM_ROT);
+            float retz = camRot.Z * 0.0174532924F;
+            float retx = camRot.X * 0.0174532924F;
+            float absx = (float)Math.Abs(Math.Cos(retx));
+            Vector3 camStuff = new Vector3((float)Math.Sin(retz) * absx * -1, (float)Math.Cos(retz) * absx, (float)Math.Sin(retx));
+            //AimPostion Result
+            RaycastResult ray = World.Raycast(camPos, camPos + camStuff * 1000, IntersectFlags.Everything);
+            Vector3 trg = ray.HitPosition;
+            return trg;
         }
+       
         void resetCamera()
         {
             if (cameraSet)
@@ -88,43 +56,46 @@ namespace AimCam
         void setCamera()
         {
             Ped player = Game.Player.Character;
-            if (!cameraSet) 
-            {
-                headCam = World.CreateCamera(player.Position, player.Rotation, 65.0f);
-                Function.Call(Hash._ATTACH_CAM_TO_PED_BONE_2, headCam, player, 12844, 0.0f, rotation, 0.0f, sidetoside, backtofront, 0.1f, true);
-                Function.Call(Hash._FREEZE_PED_CAMERA_ROTATION, player);
-                World.RenderingCamera = headCam;
-                cameraSet = true;
-            }
-            
+
             if (cameraSet)
             {
-               // Function.Call(Hash.SET_TIMECYCLE_MODIFIER, "secret_camera");
-               // Function.Call(Hash.SET_TIMECYCLE_MODIFIER_STRENGTH, 1.0f);
+                Function.Call(Hash.DISPLAY_RADAR, false);
+                // Function.Call(Hash.SET_TIMECYCLE_MODIFIER, "secret_camera");
+                // Function.Call(Hash.SET_TIMECYCLE_MODIFIER_STRENGTH, 1.0f);
                 GameplayCamera.ClampPitch(0.0f, 0.0f);
                 GameplayCamera.ClampYaw(0.0f, 0.0f);
-
                 if (player.IsAiming || player.IsReloading)
                 {
+                    Vector3 target = rayCast();
+                    float siz = target.DistanceTo(player.Position);
+                    siz = siz / 290;
+                    Function.Call(Hash._DRAW_SPHERE, target.X, target.Y, target.Z, siz, 255, 0, 0, 1.0f);
                     GameplayCamera.ClampPitch(-360.0f, 360.0f);
                     GameplayCamera.ClampYaw(-360.0f, 360.0f);
                     Function.Call(Hash.SET_CAM_AFFECTS_AIMING, headCam, false);
-                    
                 }
+            }
+            else
+            if (!cameraSet) 
+            {
+                headCam = World.CreateCamera(player.Position, player.Rotation, 85.0f);
+                Function.Call(Hash._ATTACH_CAM_TO_PED_BONE_2, headCam, player, 31086, 0f, rotation, -8f, sidetoside, backtofront, 0.09f, true);
+                
+                //Function.Call(Hash._FREEZE_PED_CAMERA_ROTATION, player);
+                headCam.NearClip = 0f;
+                World.RenderingCamera = headCam;
+                cameraSet = true;
             }
         }
         void onTick(object sender, EventArgs e)
         {
             Ped player = Game.Player.Character;
-            
 //TEMP
             if (player.Health <= deathHealth)
             {
                 resetCamera();
             }
 //\TEMP
-            if (!player.IsInVehicle())
-            {
                 if(cameraSet && World.RenderingCamera != headCam)
                 {
                     Wait(delay);
@@ -132,10 +103,27 @@ namespace AimCam
                     Wait(1);
                     setCamera();
                 }
-                if (mode)
+                
+                if (player.IsInVehicle())
+                {
+                    view = Function.Call<int>(Hash.GET_FOLLOW_VEHICLE_CAM_VIEW_MODE);
+                }
+                else if (!player.IsInVehicle())
                 {
                     view = Function.Call<int>(Hash.GET_FOLLOW_PED_CAM_VIEW_MODE);
-
+                }
+                if (!mode)
+                {
+                    if (view == replaceCam && player.Health > deathHealth)
+                    {
+                        setCamera();
+                    }
+                    if (view != replaceCam)
+                    {
+                        resetCamera();
+                    }
+                } else if (mode)
+                {
                     if (view != 4)
                     {
                         if (player.IsAiming || !player.IsAiming && player.IsReloading)
@@ -148,20 +136,6 @@ namespace AimCam
                         }
                     }
                 }
-
-                if (!mode)
-                {
-                    view = Function.Call<int>(Hash.GET_FOLLOW_PED_CAM_VIEW_MODE);
-                    if (view == replaceCam && player.Health > deathHealth)
-                    {
-                        setCamera();
-                    }
-                    if (view != replaceCam)
-                    {
-                        resetCamera();
-                    }
-                }
-            }
         }
     }
 }
